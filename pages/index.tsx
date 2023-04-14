2efd5e8ff019e6bd6a775881bf6277b258a4e90a
Lynix <81735347+lunarlynix@users.noreply.github.com>
2023-04-14 11:50:50 -0300

    LynixCA v3

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,124 +1,80 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import { useState } from 'react';
+import { useRouter } from 'next/router';
+
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+
+  // Router NextJS
+  const router = useRouter();
+  const { nfc } = router.query;
+
+
+  const [isSSA, setIsSSA] = useState(false);
+  const [mainKernelPanic, setMainKernelPanic] = useState(false);
+  const [ssaKernelPanic, setSSAKernelPanic] = useState(false);
+
+  const nfcDatabase: any = [
+    {
+      "id": "5a700528-c487-40e6-826f-b4153b309d89",
+      "event": "Lynix's Wallet Card"
+    }
+  ];
+  
+  // Check NFC Database
+  if (nfc) {
+    const nfcData = nfcDatabase.find((nfcData: any) => nfcData.id === nfc);
+    if (nfcData) {
+      console.log(nfcData.event);
+      //router.push(`/nfc/${nfcData.event}`);
+    }
+  }
+
+  const setPanicMainSite = async () => {
+    setMainKernelPanic(true);
+    // Wait 5 seconds
+    await new Promise(r => setTimeout(r, 3000));
+    // Reset panic
+    setMainKernelPanic(false);
+  }
+
+  const setSSA = async () => {
+    setIsSSA(true);
+  }
+  
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <main className="flex min-h-screen flex-col items-center sm:justify-center justify-left p-5 sm:p-24">
+      {!nfc && <>
+      {!isSSA && <pre className="w-full" style={{"fontFamily": "monospace"}}>&gt; lynix_cli --api:api.lynix.ca --allow-site-options{"\n"}
+      {"\n"}Welcome to
+      {"\n"}{"  "}_{"             "}_{"                      "}{"\n"} | |{"           "}(_){"                     "}{"\n"} | |_{"   "}_ _ __{"  "}___{"  "}__{"       "}___ __ _ {"\n"} | | | | | '_ \| \ \/ /{"      "}/ __/ _` |{"\n"} | | |_| | | | | |&gt;{"  "}&lt;{"   "}_{"  "}| (_| (_| |{"\n"} |_|\__, |_| |_|_/_/\_\ (_){"  "}\___\__,_|{"\n"}{"     "}__/ |{"                             "}{"\n"}{"    "}|___/{"              "}{"\n"}
+      {mainKernelPanic && <>
+      {"\n"}<span className="text-red-600">[KERNEL PANIC] We could not connect you to the lynix.ca website!</span>{"\n"}
+      </>}
+      {"\n"}Choose an option: {"\n"}
+      {"\n"}<span className="hover:bg-gray-100 hover:text-black cursor-pointer" onClick={() => {setPanicMainSite()}}>1. Continue to Site</span>
+      {"\n"}<span className="hover:bg-gray-100 hover:text-black cursor-pointer" onClick={() => {setSSA()}}>2. Enter Super Secret Area (Pin Required)</span>
+      {"\n"}
+      {"\n"}Home of all things infosec, programming and weird experiments.
+      {"\n"}If you'd like to get in touch with Lynix, send an email to lynix@lynix.ca
+      {"\n"}
+      </pre>}
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      {isSSA && 
+        <pre className="w-full"  style={{"fontFamily": "monospace"}}>&gt; lynix_cli --ssa{"\n"}{"\n"}Welcome to the{"\n"}{"   "}_____{"                          "}_____{"                    "}_{"                             "}{"\n"}{"  "}/ ____|{"                        "}/ ____|{"                  "}| |{"       "}/\{"                   "}{"\n"} | (___{"  "}_{"   "}_ _ __{"   "}___ _ __{"  "}| (___{"   "}___{"  "}___ _ __ ___| |_{"     "}/{"  "}\{"   "}_ __ ___{"  "}__ _ {"\n"}{"  "}\___ \| | | | '_ \ / _ \ '__|{"  "}\___ \ / _ \/ __| '__/ _ \ __|{"   "}/ /\ \ | '__/ _ \/ _` |{"\n"}{"  "}____) | |_| | |_) |{"  "}__/ |{"     "}____) |{"  "}__/ (__| | |{"  "}__/ |_{"   "}/ ____ \| | |{"  "}__/ (_| |{"\n"} |_____/ \__,_| .__/ \___|_|{"    "}|_____/ \___|\___|_|{"  "}\___|\__| /_/{"    "}\_\_|{"  "}\___|\__,_|{"\n"}{"              "}| |{"                                                                        "}{"\n"}{"              "}|_|{"                                                                       "}{"\n"}
+        {ssaKernelPanic && <>
+        {"\n"}<span className="text-red-600">[KERNEL PANIC] Failed to authenticate with Lynix API, Connection Refused!</span>{"\n"}
+        </>}
+        {"\n"}You must enter the pincode provided to you:{"\n"}
+        {"\n"}<span>[</span><input id="pin" name="pin" type="password" className="bg-black focus:outline-0"></input><span>]</span>
+        {"\n"}
+        {"\n"}<span className="hover:bg-gray-100 hover:text-black cursor-pointer" onClick={() => {setSSAKernelPanic(true)}}>[Done]</span>
+        {"\n"}</pre>
+      }
+      </>}
     </main>
   )
 }
